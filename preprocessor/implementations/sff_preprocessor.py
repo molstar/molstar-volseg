de36dc854e26a81ac069c22c0c479884381199a9
aliaksei-chareshneu <chareshneu.tech@gmail.com>
2022-02-14 16:56:22 +0300

    reorganized sff preprocessor code

--- preprocessor/implementations/sff_preprocessor.py
+++ preprocessor/implementations/sff_preprocessor.py
@@ -9,27 +9,67 @@ import numcodecs
 import h5py
 import numpy as np
 from preprocessor.interface.i_data_preprocessor import IDataPreprocessor
+from preprocessor.preprocessor import DOWNSAMPLING_STEPS
 
 
 class SFFPreprocessor(IDataPreprocessor):
-    def preprocess(self, file_path: Path) -> np.ndarray:
-        pass
+    def preprocess(self, file_path: Path):
+        '''
+        Returns processed data (zarr structure) that will be stored using db.store
+        '''
+        zarr_structure = self.__hdf5_to_zarr(file_path)
+        
+        for gr_name, gr in zarr_structure.lattice_list.groups():
+            self.__create_downsamplings(gr)
 
-    def __decode_lattice_data(self, data: str) -> str:
-        '''Decodes lattice data into just zlib-zipped data'''
-        return base64.b64decode(data)
-
-    def __decompress_lattice_data(self, decoded_data: str) -> str:
-        '''Decompresses lattice data to bytes'''
-        return zlib.decompress(decoded_data)
-
-    def __convert_bytes_to_arr(self, byteseq: bytes, dtype: str, arr_shape: Tuple[int, int, int]) -> np.ndarray:
-        '''Converts decompressed lattice date (bytes) into np arr based on dtype (sff mode) and shape (sff size)'''
+    def __lattice_data_to_np_arr(self, data: str, dtype: str, arr_shape: Tuple[int, int, int]) -> np.ndarray:
+        '''
+        Converts lattice data to np array.
+        Under the hood, decodes lattice data into zlib-zipped data, decompress it to bytes,
+        and converts to np arr based on dtype (sff mode) and shape (sff size)
+        '''
+        decoded_data = base64.b64decode(data)
+        byteseq = zlib.decompress(decoded_data)
         return np.frombuffer(byteseq, dtype=dtype).reshape(arr_shape)
 
-    def __downsample_arr(self, arr: np.ndarray) -> np.ndarray:
-        '''Returns downsampled (e.g. every other value) array'''
+    def __downsample_data(self, arr: np.ndarray, rate) -> np.ndarray:
+        '''Returns downsampled (e.g. every other value) np array'''
+        # return block_reduce(arr, block_size=(2, 2, 2), func=np.max)
         pass
+    
+    def __create_downsamplings(self, gr):
+        # TODO create x1 "down"sampling too
+        data = self.__lattice_data_to_np_arr(
+            gr.data[0],
+            gr.mode[0],
+            (gr.size.cols[...], gr.size.rows[...], gr.size.sections[...]))
+
+        downsampled_data_group = gr.create_group('downsampled_data')
+        # iteratively downsample data, create arr for each dwns. level and store data 
+        ratios = 2 ** np.arange(1, DOWNSAMPLING_STEPS + 1)
+        for rate in ratios:
+            self.__create_downsampling(data, gr, rate, downsampled_data_group)
+        # TODO: figure out compression/filters: b64 encoded zlib-zipped .data is just 8 bytes
+        # downsamplings sizes in raw uncompressed state are much bigger 
+        # TODO: figure out what to do with chunks - currently they are not used
+
+    def __create_downsampling(self, original_data, rate, downsampled_data_group):
+        '''Creates zarr array (dataset) filled with downsampled data'''
+        downsampled_data = self.__downsample_data(original_data, rate)
+        zarr_arr = downsampled_data_group.create_dataset(
+            str(rate),
+            shape=downsampled_data.shape,
+            dtype=downsampled_data.dtype)
+        zarr_arr[...] = downsampled_data
+
+    def __hdf5_to_zarr(self, file_path: Path):
+        '''
+        Returns zarr structure mirroring that of hdf5
+        '''
+        hdf5_file: h5py._hl.files.File = h5py.File(file_path, mode='r')
+        hdf5_file.visititems(self.__visitor_function)
+        hdf5_file.close()
+        # return zarr.open_group()
 
     def __visitor_function(self, name: str, node: h5py._hl.dataset.Dataset) -> None:
         '''
@@ -37,7 +77,8 @@ class SFFPreprocessor(IDataPreprocessor):
         with addtional items (e.g. downsampled_data group, possibly metadata)
         '''
         # TODO: may not work as it is a method, not a function. Check?
-        # TODO: uncomment and rework code (taken from preprocessor.py) so that it works in class
+        # TODO: rework such that zarr structure is created in memory
+        # 
         # emdb_seg_id = hdf5_file.filename.split('/')[-1].split('.')[0]
         # root_path = PATH_TO_OUTPUT_DIR + emdb_seg_id
         # if isinstance(node, h5py.Dataset):
