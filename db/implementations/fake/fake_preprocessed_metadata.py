0c28138efcb3a5a1a0288900afce0a88331b217d
raviramos <48851099+raviramos@users.noreply.github.com>
2022-03-14 09:00:20 +0100

    fixed FakePreprocessedMetadata not implementing all abstract methods

--- db/implementations/fake/fake_preprocessed_metadata.py
+++ db/implementations/fake/fake_preprocessed_metadata.py
@@ -1,7 +1,27 @@
+from typing import List
+
 from db.interface.i_preprocessed_medatada import IPreprocessedMetadata
 
 
 class FakePreprocessedMetadata(IPreprocessedMetadata):
+    def segmentation_lattice_ids(self) -> List[int]:
+        return [0, 0, 0]
+
+    def segmentation_downsamplings(self, lattice_id: int) -> List[int]:
+        return [0, 0, 0]
+
+    def volume_downsamplings(self) -> List[int]:
+        [0]
+
+    def origin(self) -> List[float]:
+        pass
+
+    def voxel_size(self, downsampling_rate: int) -> List[float]:
+        return [0, 0, 0]
+
+    def grid_dimensions(self) -> List[int]:
+        return [0, 0, 0]
+
     def lattice_ids(self) -> list[int]:
         return [0]
 
