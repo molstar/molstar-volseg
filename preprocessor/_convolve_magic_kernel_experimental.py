1290bc3bd08225dea8a01fc5bd5441c6f39c36f6
aliaksei-chareshneu <chareshneu.tech@gmail.com>
2022-04-04 17:11:09 +0200

    try different versions of convolution func

--- preprocessor/_convolve_magic_kernel_experimental.py
+++ preprocessor/_convolve_magic_kernel_experimental.py
@@ -1,5 +1,11 @@
 import numpy as np
-from scipy import ndimage
+from scipy import ndimage, signal
+
+# TODO: generate kernel either myself or smart function
+# TODO: apply on actual code (make comparison of outputs by copying old images
+# and creating a separate func + conditional in (as it accepts 3D kernel, not 1D as the current func)
+# 
+
 k = np.array([
     [1, 1, 1, 1, 1],
     [1, 4, 4, 4, 1],
@@ -8,9 +14,31 @@ k = np.array([
     [1, 1, 1, 1, 1]
 ])
 
-a = np.arange(25).reshape(5,5)
-# TODO: Divide each by 16 after conv!
-c = ndimage.convolve(a, k, mode='constant', cval=0.0)
-print(c)
+k = np.arange(125).reshape(5,5,5)
+
+# k = np.array(
+#     [
+#         [
+#             [1]
+#         ],
+#         [
+
+#         ],
+#         [
+
+#         ]   
+#     ]
+# )
+
+k = (1/k.sum()) * k
+
+a = np.arange(125).reshape(5,5,5).astype(np.float)
+# a = np.random.normal(0, 10, (5, 5, 5))#.astype(np.int)
+
+c_ndimage = ndimage.convolve(a, k, mode='constant', cval=0.0)
+c_convolve = signal.convolve(a, k, mode='same', method='fft')
+print(a)
+print(c_ndimage[::2, ::2])
+print(c_convolve[::2, ::2])
 
 # TODO: try 3d kernel
