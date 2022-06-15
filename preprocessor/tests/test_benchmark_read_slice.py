c8173358350f568c04c42c0b03c9868d2e7d6e89
Aliaksei Chareshneu <chareshneu.tech@gmail.com>
2022-06-15 16:24:51 +0200

    simple benchmarking

--- preprocessor/tests/test_benchmark_read_slice.py
+++ preprocessor/tests/test_benchmark_read_slice.py
@@ -2,19 +2,69 @@ import pytest
 
 from db.implementations.local_disk.local_disk_preprocessed_db import LocalDiskPreprocessedDb
 
+@pytest.fixture(scope='function')
+def aio_benchmark(benchmark):
+    import asyncio
+    import threading
+    
+    class Sync2Async:
+        def __init__(self, coro, *args, **kwargs):
+            self.coro = coro
+            self.args = args
+            self.kwargs = kwargs
+            self.custom_loop = None
+            self.thread = None
+        
+        def start_background_loop(self) -> None:
+            asyncio.set_event_loop(self.custom_loop)
+            self.custom_loop.run_forever()
+        
+        def __call__(self):
+            evloop = None
+            awaitable = self.coro(*self.args, **self.kwargs)
+            try:
+                evloop = asyncio.get_running_loop()
+            except:
+                pass
+            if evloop is None:
+                return asyncio.run(awaitable)
+            else:
+                if not self.custom_loop or not self.thread or not self.thread.is_alive():
+                    self.custom_loop = asyncio.new_event_loop()
+                    self.thread = threading.Thread(target=self.start_background_loop, daemon=True)
+                    self.thread.start()
+                
+                return asyncio.run_coroutine_threadsafe(awaitable, self.custom_loop).result()
+    
+    def _wrapper(func, *args, **kwargs):
+        if asyncio.iscoroutinefunction(func):
+            benchmark(Sync2Async(func, *args, **kwargs))
+        else:
+            benchmark(func, *args, **kwargs)
+
+    return _wrapper
+
 @pytest.mark.parametrize("key", ['emd-1832', 'emd-99999'])
 # TODO: box sizes should be computed using func we had from preprocessor\tests\test_slicing_methods_benchmarking.py
 @pytest.mark.parametrize("box", [((10, 10, 10), (20, 20, 20)), ((10, 10, 10), (30, 30, 30))])
 # TODO: downsampling ratios from metadata
 @pytest.mark.parametrize("down_sampling_ratio", [1, 2])
 @pytest.mark.asyncio
-async def test_an_async_function(key, box, down_sampling_ratio):
-    db = LocalDiskPreprocessedDb()
-    result = await db.read_slice(
-        namespace='emdb',
-        key=key,
-        lattice_id=0,
-        down_sampling_ratio=down_sampling_ratio,
-        box=box,
-        mode='zarr_colon'
-    )
+async def test_t(aio_benchmark, key, box, down_sampling_ratio):
+    @aio_benchmark
+    async def _():
+        db = LocalDiskPreprocessedDb()
+        result = await db.read_slice(
+            namespace='emdb',
+            key=key,
+            lattice_id=0,
+            down_sampling_ratio=down_sampling_ratio,
+            box=box,
+            mode='zarr_colon'
+        )
+
+        
+    
+
+# https://stackoverflow.com/questions/35110308/grouping-parametrized-benchmarks-with-pytest
+# How to parametrize benchmarking function (not async, but yet)
