15bcd1b63cb4e9595717cfa5053a7de222e8c13a
Aliaksei Chareshneu <chareshneu.tech@gmail.com>
2022-06-20 16:51:48 +0200

    gitignore; creating fake ccp4 map with dtype as param;

--- preprocessor/src/tools/create_fake_ccp4_map/create_fake_ccp4_map.py
+++ preprocessor/src/tools/create_fake_ccp4_map/create_fake_ccp4_map.py
@@ -1,14 +1,13 @@
 from pathlib import Path
 import gemmi
-from matplotlib.pyplot import grid
 import numpy as np
 
 from preprocessor.src.preprocessors.implementations.sff.preprocessor._volume_map_methods import read_and_normalize_map
 
 
-def create_fake_ccp4_map(grid_size: tuple[int, int, int], filepath: Path):
+def create_fake_ccp4_map(grid_size: tuple[int, int, int], filepath: Path, dtype):
     ccp4 = gemmi.Ccp4Map()
-    ccp4.grid = gemmi.FloatGrid(np.zeros(grid_size, dtype=np.float32))
+    ccp4.grid = gemmi.FloatGrid(np.zeros(grid_size, dtype=dtype))
 
     # TODO: these two is relevant?
     ccp4.grid.unit_cell.set(20, 20, 20, 90, 90, 90)
@@ -21,9 +20,9 @@ def create_fake_ccp4_map(grid_size: tuple[int, int, int], filepath: Path):
 
 
 if __name__ == '__main__':
-    large_map_filepath = Path('preprocessor/data/raw_input_files/emdb/emd-88888/fake_large_map.ccp4')
     grid_size = (1000, 1000, 1000)
-    create_fake_ccp4_map(grid_size=grid_size, filepath=large_map_filepath)
+    large_map_filepath = Path(f'preprocessor/data/raw_input_files/emdb/emd-88888/fake_large_map.ccp4')
+    create_fake_ccp4_map(grid_size=grid_size, filepath=large_map_filepath, dtype=np.float32)
 
     arr = read_and_normalize_map(large_map_filepath)
 
