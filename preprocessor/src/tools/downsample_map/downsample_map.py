0dfccd821248e77702a473bac2afa5cb22993fbf
Aliaksei Chareshneu <chareshneu.tech@gmail.com>
2022-07-11 10:02:30 +0200

    switch to mrcfile mmap, dask arrs, and dask convolve

--- preprocessor/src/tools/downsample_map/downsample_map.py
+++ preprocessor/src/tools/downsample_map/downsample_map.py
@@ -6,15 +6,17 @@ from scipy import signal, ndimage
 import numpy as np
 from preprocessor.src.preprocessors.implementations.sff.preprocessor.downsampling.downsampling import generate_kernel_3d_arr
 from timeit import default_timer as timer
+import dask.array as da
+from dask_image.ndfilters import convolve as dask_convolve
+
 
 def downsample_map(input_path: Path, output_path: Path, size_limit: int):
     kernel = generate_kernel_3d_arr(list(DOWNSAMPLING_KERNEL))
     
-    # open with mmap if too big
-    with mrcfile.open(str(input_path.resolve())) as mrc_original:
+    with mrcfile.mmap(str(input_path.resolve())) as mrc_original:
         original_cella = mrc_original.header.cella
         original_nstart = mrc_original.nstart
-        data: np.ndarray = mrc_original.data
+        data: np.memmap = mrc_original.data
         print('original header')
         mrc_original.print_header()
 
@@ -22,10 +24,13 @@ def downsample_map(input_path: Path, output_path: Path, size_limit: int):
     print(f'{input_path.name} - size of original data: ~ {size / 1000000} MB')
     downsampled_data = data
 
+    downsampled_data = da.from_array(downsampled_data)
+
     num_steps = 0
     while size > size_limit:
         num_steps = num_steps + 1
-        downsampled_data = ndimage.convolve(downsampled_data, kernel, mode='mirror', cval=0.0)
+        # ndimage.convolve(downsampled_data, kernel, mode='mirror', cval=0.0, output=downsampled_data)
+        downsampled_data = dask_convolve(downsampled_data, kernel, mode='mirror', cval=0.0)
         downsampled_data = downsampled_data[::2, ::2, ::2]
         size = downsampled_data.nbytes
         print(f'downsampled to: {size / 1000000} MB')
@@ -34,7 +39,6 @@ def downsample_map(input_path: Path, output_path: Path, size_limit: int):
         nstart_divisor = 2 ** num_steps
         mrc.set_data(downsampled_data)
         mrc.header.cella = original_cella
-        # TODO: negative -55.5 => -55; positive 55.5 => 56. Issue?
         mrc.nstart = (
             math.floor(original_nstart.x / nstart_divisor),
             math.floor(original_nstart.y / nstart_divisor),
@@ -55,10 +59,11 @@ def _check_if_map_is_ok(map_path: Path):
 
         
 if __name__ == '__main__':
-    INPUT_MAP = 'preprocessor\data\sample_volumes\emdb_sff\emd_9199.map'
+    INPUT_MAP = 'preprocessor\data\sample_volumes\emdb_sff\emd_13856.map'
+    # INPUT_MAP = 'preprocessor\data\sample_volumes\emdb_sff\emd_9199.map'
     # INPUT_MAP = 'preprocessor\data\sample_volumes\emdb_sff\EMD-1832.map'
-    OUTPUT_MAP = 'temp/downsampled_9199.map'
-    SIZE_LIMIT = 1 * (10**9)
+    OUTPUT_MAP = 'temp/downsampled_13856.map'
+    SIZE_LIMIT = 9 * (10**9)
     downsample_map(input_path=Path(INPUT_MAP), output_path=Path(OUTPUT_MAP), size_limit=SIZE_LIMIT)
     _check_if_map_is_ok(Path(OUTPUT_MAP))
     
