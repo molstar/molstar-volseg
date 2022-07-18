dcc3d9d5169e66bd9299347d4642366881f38402
Aliaksei Chareshneu <chareshneu.tech@gmail.com>
2022-07-18 16:32:05 +0200

    adjust to work on dask arrs

--- preprocessor/src/tools/quantize_data/quantize_data.py
+++ preprocessor/src/tools/quantize_data/quantize_data.py
@@ -2,11 +2,12 @@
 from pathlib import Path
 import mrcfile
 import numpy as np
+import dask.array as da
 
 def read_ccp4_map_mrcfile(map_path: Path) -> np.ndarray:
     return mrcfile.read(str(map_path.resolve()))
 
-def quantize_data(data: np.ndarray, output_dtype) -> np.ndarray:
+def quantize_data(data: da.Array, output_dtype) -> np.ndarray:
     bits_in_dtype = output_dtype().itemsize * 8
     num_steps = 2**bits_in_dtype - 1
     
@@ -23,10 +24,11 @@ def quantize_data(data: np.ndarray, output_dtype) -> np.ndarray:
     np.divide(data, delta, out=quantized)
     quantized = quantized.astype(dtype=output_dtype)
 
-    return quantized
+    return quantized.compute()
 
 if __name__ == '__main__':
     INPUT_MAP_PATH = Path('preprocessor\data\sample_volumes\emdb_sff\EMD-1832.map')
     data = read_ccp4_map_mrcfile(INPUT_MAP_PATH)
+    data = da.from_array(data)
     quantized_data = quantize_data(data=data, output_dtype=np.uint8)
     print(quantized_data)
