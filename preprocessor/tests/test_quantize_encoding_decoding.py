84ad4521506e974414d31b45a8e2dbdc6aa935bd
Aliaksei Chareshneu <chareshneu.tech@gmail.com>
2022-07-21 19:02:16 +0200

    atol formula

--- preprocessor/tests/test_quantize_encoding_decoding.py
+++ preprocessor/tests/test_quantize_encoding_decoding.py
@@ -4,18 +4,20 @@ import numpy as np
 
 from preprocessor.src.tools.quantize_data.quantize_data import decode_quantized_data, quantize_data
 
+def calculate_atol(data_dict: dict):
+    atol = data_dict['max'] - data_dict['min'] / data_dict['num_steps'] / 2
+    if isinstance(atol, da.Array):
+        return atol.compute()
+    return atol
+
 class TestEncodingsIntervalQuantization(unittest.TestCase):
     def test(self):
 
         test_suite = [
             (np.random.rand(100) * 100, np.uint8),
             (np.random.rand(100) * 100, np.uint16),
-            (np.random.rand(100) * 100, np.int8),
-            (np.random.rand(100) * 100, np.int16),
             (da.from_array(np.random.rand(100) * 100), np.uint8),
             (da.from_array(np.random.rand(100) * 100), np.uint16),
-            (da.from_array(np.random.rand(100) * 100), np.int8),
-            (da.from_array(np.random.rand(100) * 100), np.int16),
         ]
 
         for test_arr, dtype in test_suite:
@@ -25,5 +27,6 @@ class TestEncodingsIntervalQuantization(unittest.TestCase):
             encoded = quantize_data(data=test_arr, output_dtype=dtype)
             decoded = decode_quantized_data(encoded)
             
-            self.assertTrue(np.allclose(test_arr, decoded, atol=1.1 * (high - low) / steps),
+            atol = calculate_atol(encoded)
+            self.assertTrue(np.allclose(test_arr, decoded, atol=atol),
                 msg=f'{test_arr}, {decoded}')
