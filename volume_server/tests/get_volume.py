26103593b90b9ae3accd51c7846d68e63827fde4
mc-crocodile <crocodile@monstercrocodile.com>
2022-08-06 11:54:50 +0200

    patched get volume test to print length of response

--- volume_server/tests/get_volume.py
+++ volume_server/tests/get_volume.py
@@ -10,11 +10,11 @@ class FetchVolumeTest(ServerTestBase):
     def test(self):
         try:
             with self.server.run_in_thread():
-                r = requests.get(f'{self.serverUrl()}/v1/emdb/emd-1832/box/0/-100/-100/-100/50/50/50/1000')
+                r = requests.get(f'{self.serverUrl()}/v1/emdb/emd-1832/box/0/-20/-20/-20/20/20/20/1000')
                 self.assertEqual(r.status_code, 200)
-                body: dict = dict(r.json())
+                body = r.text
                 self.assertIsNotNone(body)
-                print("Received body: " + str(body))
+                print("Received body of len : " + str(len(body)))
 
         finally:
             pass
