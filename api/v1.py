fa91ecb392db0a7ce2445833c76c8b721b837b53
mc-crocodile <crocodile@monstercrocodile.com>
2022-08-06 11:24:01 +0200

    added route for getting cell to api

--- api/v1.py
+++ api/v1.py
@@ -27,6 +27,25 @@ def configure_endpoints(app: FastAPI, volume_server: IVolumeServer):
         # return {}
         return Response(response, headers={"Content-Disposition": f'attachment;filename="{id}.bcif"'})
 
+    @app.get("/v1/{source}/{id}/cell/{segmentation}/{max_points}")
+    async def get_cell(
+            source: str,
+            id: str,
+            segmentation: int,
+            a1: float,
+            a2: float,
+            a3: float,
+            b1: float,
+            b2: float,
+            b3: float,
+            max_points: Optional[int] = 0
+    ):
+        request = VolumeRequest(source, id, segmentation, a1, a2, a3, b1, b2, b3, max_points)
+        response = await volume_server.get_volume(request)
+
+        # return {}
+        return Response(response, headers={"Content-Disposition": f'attachment;filename="{id}.bcif"'})
+
     @app.get("/v1/{source}/{id}/metadata")
     async def get_metadata(
             source: str,
