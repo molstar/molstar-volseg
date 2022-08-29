b052c0a169806a0dc1e4770ad43a71e49821eb3e
Aliaksei Chareshneu <chareshneu.tech@gmail.com>
2022-08-29 14:40:07 +0200

    update benchmarking code

--- preprocessor/src/tools/run_amira_benchmarking/run_amira_benchmarking.py
+++ preprocessor/src/tools/run_amira_benchmarking/run_amira_benchmarking.py
@@ -8,6 +8,8 @@ import time
 import multiprocessing as mp
 import numpy as np
 
+INPUT_DATA = None
+
 # Methodology:
 # exec(open("C:\\Users\\chere\\Work\\cellstar-volume-server\\preprocessor\\src\\tools\\run_amira_benchmarking\\run_amira_benchmarking.py").read())
 # works and have access to hx_project variable
@@ -18,23 +20,6 @@ import numpy as np
 # Open an input file
 # Paste code for benchmarking all methods
 
-# @profile
-def benchmark_non_local_means_filter():
-    # here goes the method setup
-
-    # hx_project.create('HxVolumeRenderingSettings')
-    # hx_project.create('HxVolumeRender2')
-    # settings = hx_project.get('Volume Rendering Settings')
-    # volume_render = hx_project.get('Volume Rendering')
-    # input_data = hx_project.get('EMD-1832.mrc')
-
-    # settings.ports.data.connect(input_data)
-    # settings.fire()
-    # volume_render.ports.volumeRenderingSettings.connect(settings)
-    # volume_render.fire()
-    a = hx_project.get('Non-Local Means Filter')
-    a.ports.doIt.was_hit = True
-    a.fire()
 
 # def compute_isosurface_threshold():
 #     input_data = hx_project.get('EMD-1832.mrc')
@@ -59,6 +44,18 @@ def benchmark_isosurface():
     isosurface.ports.threshold.value = iso_threshold
     isosurface.ports.doIt.was_hit = True
     isosurface.fire()
+
+def benchmark_volume_rendering():
+    hx_project.create('HxVolumeRenderingSettings')
+    hx_project.create('HxVolumeRender2')
+    settings = hx_project.get('Volume Rendering Settings')
+    volume_render = hx_project.get('Volume Rendering')
+    input_data = hx_project.get(INPUT_DATA)
+
+    settings.ports.data.connect(input_data)
+    settings.fire()
+    volume_render.ports.volumeRenderingSettings.connect(settings)
+    volume_render.fire()
 
 def benchmark_method_2():
     pass
