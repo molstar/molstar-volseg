3c513d5188ae990aae1e89f13c6694af3b35f1ac
Aliaksei Chareshneu <chareshneu.tech@gmail.com>
2022-09-05 18:24:44 +0200

    script for generating db with 5 entries (3 frontend examples + 2 from volume browser)

---
+++ preprocessor/test_db_generation.sh
@@ -0,0 +1,26 @@
+# python "preprocessor/main.py" "--db_path" "db" \
+#         "--single_entry" "preprocessor/data/raw_input_files/empiar/empiar-10070" \
+#         "--entry_id" "empiar-10070" \
+#         "--source_db" "empiar" \
+#         "--force_volume_dtype" "u1" \
+
+# python "preprocessor/main.py" "--db_path" "db" \
+#         "--single_entry" "preprocessor/data/raw_input_files/emdb/emd-1832" \
+#         "--entry_id" "emd-1832" \
+#         "--source_db" "emdb" \
+
+# python "preprocessor/main.py" "--db_path" "db" \
+#         "--single_entry" "preprocessor/data/raw_input_files/emdb/emd-2847" \
+#         "--entry_id" "emd-2847" \
+#         "--source_db" "emdb" \
+
+# python "preprocessor/main.py" "--db_path" "db" \
+#         "--single_entry" "preprocessor/data/raw_input_files/emdb/emd-6338" \
+#         "--entry_id" "emd-6338" \
+#         "--source_db" "emdb" \
+
+python "preprocessor/main.py" "--db_path" "db" \
+        "--single_entry" "preprocessor/data/raw_input_files/emdb/emd-99999" \
+        "--entry_id" "emd-99999" \
+        "--source_db" "emdb" \
+        "--force_volume_dtype" "u1" \
