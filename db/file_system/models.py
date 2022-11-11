10859bb20e68870d7c856fff97ff2c821c234768
Aliaksei Chareshneu <chareshneu.tech@gmail.com>
2022-11-11 13:49:19 +0000

    correct field names in FileSystemVolumeMedatada

--- db/file_system/models.py
+++ db/file_system/models.py
@@ -14,10 +14,10 @@ class FileSystemVolumeMedatada(VolumeMetadata):
         return self.raw_metadata
     
     def db_name(self) -> str:
-        return self.raw_metadata["general"]["db_name"]
+        return self.raw_metadata["general"]["source_db_name"]
 
     def entry_id(self) -> str:
-        return self.raw_metadata["general"]["entry_id"]
+        return self.raw_metadata["general"]["source_db_id"]
 
     def segmentation_lattice_ids(self) -> List[int]:
         return self.raw_metadata["segmentation_lattices"]["segmentation_lattice_ids"]
