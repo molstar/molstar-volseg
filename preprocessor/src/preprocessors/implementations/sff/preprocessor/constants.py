524816d3681d42d7a85ba522fd456cb6958e4df8
dsehnal <david.sehnal@gmail.com>
2022-12-12 15:30:35 +0100

    more docs around

--- preprocessor/src/preprocessors/implementations/sff/preprocessor/constants.py
+++ preprocessor/src/preprocessors/implementations/sff/preprocessor/constants.py
@@ -22,7 +22,7 @@ REAL_MAP_FILEPATH = Path('test-data/preprocessor/sample_volumes/emdb_sff/EMD-183
 # NOTE: inside this folder, there will be subfolder with name = last path component of db_path
 TEMP_ZARR_HIERARCHY_STORAGE_PATH = Path('test-data/preprocessor/temp_zarr_hierarchy_storage')
 
-PARAMETRIZED_DBS_INPUT_PARAMS_FILEPATH = Path('parametrized_dbs_input_params.txt')
+PARAMETRIZED_DBS_INPUT_PARAMS_FILEPATH = Path('test-data/parametrized_dbs_input_params.txt')
 
 RAW_INPUT_FILES_DIR = Path('test-data/preprocessor//raw_input_files')
 DEFAULT_DB_PATH = Path('test-data/db') 
