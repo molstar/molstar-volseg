c92c8cba6b664b7b51eabfb9ec9719e75cbe7674
Aliaksei Chareshneu <chareshneu.tech@gmail.com>
2024-06-03 07:51:24 +0000

    fix path

--- vs_toolkit/tests/constants.py
+++ vs_toolkit/tests/constants.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
 VS_TOOLKIT_PATH = Path("vs_toolkit/vs_toolkit.py")
-DB_PATH_FOR_VS_TOOLKIT_TESTS = Path("/mnt/data_backup_ceph/test_db_build_by_script")
+DB_PATH_FOR_VS_TOOLKIT_TESTS = Path("preprocessor/temp/test_db")
