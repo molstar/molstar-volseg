d0b5a531120dec0b1744dbdc3cf385e79fa2f445
Aliaksei Chareshneu <chareshneu.tech@gmail.com>
2024-06-04 12:09:02 +0000

    fix, start changing build shell scripts

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -2,14 +2,15 @@
 TIMESTAMP=`date '+%Y-%m-%d-%H-%M-%S'`
 echo $TIMESTAMP
 
-cd /sw/cellstar-volume-server
+cd /sw/molstar-volseg
 git reset --hard origin/master
 git pull
 
 source /sw/mambaforge/bin/activate cellstar-volume-server
 mamba env update --file environment.yaml --prune
 
-pip install -e .
+# pip install -e .
 
-python preprocessor/src/tools/deploy_db/build_and_deploy.py "$@" &> /sw/log_$TIMESTAMP.txt 
+python preprocessor/cellstar_preprocessor/tools/deploy_db/build.py > "$@" &> /sw/log_$TIMESTAMP.txt 
+# python preprocessor/src/tools/deploy_db/build_and_deploy.py "$@" &> /sw/log_$TIMESTAMP.txt 
 
