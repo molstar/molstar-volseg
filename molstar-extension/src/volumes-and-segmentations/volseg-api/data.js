2c0d40b1213d20d7a9fa3e6f1176e7e878919b24
Aliaksei Chareshneu <chareshneu.tech@gmail.com>
2024-06-06 14:24:00 +0200

    remove js files from src

--- molstar-extension/src/volumes-and-segmentations/volseg-api/data.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * Copyright (c) 2018-2024 mol* contributors, licensed under MIT, See LICENSE file for more info.
- *
- * @author Adam Midlik <midlik@gmail.com>
- * @author Aliaksei Chareshneu <chareshneu.tech@gmail.com>
- */
-Object.defineProperty(exports, "__esModule", { value: true });
