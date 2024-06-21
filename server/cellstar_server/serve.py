b10e1ed05218ca65faec4d901702ece9a5a9953d
Aliaksei Chareshneu <chareshneu.tech@gmail.com>
2024-06-21 08:01:59 +0200

    deployment of api with ssl certificates

--- server/cellstar_server/serve.py
+++ server/cellstar_server/serve.py
@@ -1,10 +1,25 @@
+import argparse
+from pathlib import Path
 import uvicorn
 from cellstar_server.app.settings import settings
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ssl_keyfile", type=str)
+    parser.add_argument("--ssl_certfile", type=str)
+    args = parser.parse_args()
+    
     print("Mol* Volume Server")
     print(settings.dict())
-
-    uvicorn.run(
-        app="main:app", host=settings.HOST, port=settings.PORT, reload=settings.DEV_MODE
-    )
+    if args.ssl_certfile and args.ssl_keyfile:
+        uvicorn.run(
+               app="main:app", host=settings.HOST, port=settings.PORT, reload=settings.DEV_MODE,
+               ssl_keyfile=str(Path(args.ssl_keyfile).resolve()),
+               ssl_certfile=str(Path(args.ssl_certfile).resolve())
+               )
+    if not args.ssl_certfile and not args.ssl_keyfile:
+        uvicorn.run(
+            app="main:app", host=settings.HOST, port=settings.PORT, reload=settings.DEV_MODE
+        )
+    else:
+        print("ssl_certfile or ssl_keyfile was not provided")
