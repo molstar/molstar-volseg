e8bf8df75e5bf055e5161d064fa761c0b7d1f283
Aliaksei Chareshneu <chareshneu.tech@gmail.com>
2024-07-11 14:18:33 +0200

    fix

--- preprocessor/cellstar_preprocessor/tools/convert_image/convert_image.py
+++ preprocessor/cellstar_preprocessor/tools/convert_image/convert_image.py
@@ -12,8 +12,3 @@ def convert_image(inp: Path, out: Path):
     # print(rgb_im.info['dpi'], 'OUTPUT DPI')
     print(1)
     
-if __name__ == '__main__':
-    dir = Path('C:/Users/chere/OneDrive - MUNI/Figures/raw')
-    outdir = Path('C:/Users/chere/OneDrive - MUNI/Figures/JPEG')
-    for p in dir.glob("*"):
-        convert_image(p, outdir / (p.stem + '.jpeg'))
